5a14dc5fa2d8c926c1b9bf39ffbe2c6b16a53b69
riveur <riveur.cpy@gmail.com>
2024-04-18 18:08:30 +0200

    [frontend] refactor: redesign home page

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,16 +1,66 @@
-import { buttonVariants } from "@/components/ui/button";
+import { ToggleThemeButton } from "@/components/common/toggle-theme-button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-12">
-      <h1 className="text-4xl font-bold">Welcome to the Next.js + AdonisJs Starter</h1>
-      <div className="flex gap-8">
-        <Image alt="Next.js Logo" src="/next.svg" width={200} height={200} />
-        <Image alt="AdonisJs Logo" src="/adonis.svg" width={200} height={200} />
+    <main className="min-h-screen flex flex-col items-center justify-center gap-12">
+      <h1 className="text-7xl md:text-9xl font-bold">
+        <span className="text-[#5a46ff]">Ado</span>
+        <span>next</span>
+      </h1>
+      <div className="flex gap-4 justify-start">
+        <Image src="/next.svg" alt="Next.js Logo" className="dark:invert" width={100} height={100} />
+        <Image src="/adonis.svg" alt="AdonisJS Logo" width={80} height={80} />
       </div>
-      <Link href="/protected" className={buttonVariants({ variant: "link" })}>Navigate to protected route</Link>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Link href="/login" className="group">
+          <Card className="h-full group-hover:-translate-y-1 transition-all">
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <CardDescription>
+                Access to protected routes
+              </CardDescription>
+            </CardContent>
+          </Card>
+        </Link>
+        <Link href="/register" className="group">
+          <Card className="h-full group-hover:-translate-y-1 transition-all duration-75">
+            <CardHeader>
+              <CardTitle>Register</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <CardDescription>
+                Create an account
+              </CardDescription>
+            </CardContent>
+          </Card>
+        </Link>
+        <Link href="/protected" className="group">
+          <Card className="h-full group-hover:-translate-y-1 transition-all duration-75">
+            <CardHeader>
+              <CardTitle>Protected route</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <CardDescription>
+                Example of a protected route
+              </CardDescription>
+            </CardContent>
+          </Card>
+        </Link>
+        <Card className="h-full hover:-translate-y-1 transition-all duration-75">
+          <CardHeader>
+            <CardTitle>Toggle theme</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ToggleThemeButton />
+          </CardContent>
+        </Card>
+      </div>
     </main>
   );
 }

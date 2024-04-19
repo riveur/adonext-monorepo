8490274f95a83e04c8756fec6eb46625d35f3d41
riveur <riveur.cpy@gmail.com>
2024-04-19 18:17:32 +0200

    [frontend] feat(types): add PageProps type

---
+++ apps/frontend/src/types/index.ts
@@ -0,0 +1,8 @@
+import { User } from "@/lib/validation";
+
+export type PageProps<Params extends Record<string, string> = {}> = {
+  params: Params;
+  searchParams: Record<string, string | string[] | undefined>;
+}
+
+export type AuthenticatedPageProps<Params extends Record<string, string> = {}> = PageProps<Params> & { user: User }
